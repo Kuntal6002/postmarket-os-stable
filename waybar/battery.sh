7f7bfdecf611dd338edf5971f9208547ae26049f
Kuntal Das <kuntdas787@gmail.com>
2026-05-24 22:48:12 +0530

    update battery script to show accurate information

--- waybar/battery.sh
+++ waybar/battery.sh
@@ -2,6 +2,12 @@
 cap=$(cat /sys/class/power_supply/bq27411-0/capacity)
 status=$(cat /sys/class/power_supply/bq27411-0/status)
 
+if [ "$cap" -gt 95 ] && [ "$status" = "Charging" ]; then
+    cap=$(awk -v now=$(cat /sys/class/power_supply/bq27411-0/charge_now) \
+              -v design=$(cat /sys/class/power_supply/bq27411-0/charge_full_design) \
+              'BEGIN {printf "%d", (now/design)*100}')
+fi
+
 if [ "$status" = "Charging" ]; then
     echo "⚡ ${cap}%"
 else
